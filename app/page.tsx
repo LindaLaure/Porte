299eb1ce2a62f89bc49f463f91b239531e4eb687
Beerus243 <malangafabrice@gmail.com>
2024-10-15 13:07:35 +0100

    page tsx outside

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Header from './components/Header';
+import Bio from './components/Bio';
+import Projects from './components/Projects';
+import Technologies from './components/Technologies';
+import Footer from './components/Footer';
+import './Styles/globals.css';
+import './Styles/responsive.css';
+import Parcours from './components/Parcours';
+
+const Homepage = () => (
+  <>
+    <Header />
+    <Bio />
+    <Parcours />
+    <Technologies />
+    <Footer />
+  </>
+);
+
+export default Homepage;
